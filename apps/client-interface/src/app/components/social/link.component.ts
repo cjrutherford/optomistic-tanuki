330cda6c2341a4953b582e48e570b69e6d3da6fe
Chris Rutherford <therealchrisrutherford@gmail.com>
2024-12-01 14:24:58 -0500

    updated attachment and link components to be filled in with stubbed structure

--- apps/client-interface/src/app/components/social/link.component.ts
+++ apps/client-interface/src/app/components/social/link.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-link',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatInputModule, MatButtonModule, MatCardModule],
   templateUrl: './link.component.html',
   styleUrl: './link.component.scss',
 })
-export class LinkComponent {}
+export class LinkComponent {
+  linkForm = new FormGroup({
+    url: new FormControl(''),
+    description: new FormControl('')
+  });
+
+  previewLink() {
+    // Logic to preview the link
+  }
+
+  addLink() {
+    // Logic to add the link
+  }
+}
